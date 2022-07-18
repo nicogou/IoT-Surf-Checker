fb034e49d6b3cbcc763ce2bf31edc8294449e6ee
nicogou <33235852+nicogou@users.noreply.github.com>
2022-07-18 20:10:11 +0200

    Update Windows script to download library dependencies

--- update-scripts/Windows/update-script.cmd
+++ update-scripts/Windows/update-script.cmd
@@ -2,13 +2,20 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET me=%~n0
 SET parent=%~dp0
+SET target=arduino:samd:nano_33_iot
 arduino-cli config init
 arduino-cli core update-index
 arduino-cli core install arduino:samd
 IF %ERRORLEVEL% NEQ 0 (
   REM Arduino SAMD Core has not been installed properly.
 )
-arduino-cli board list
+
+arduino-cli lib update-index
+arduino-cli lib install FastLED WiFiNINA_Generic WiFiManager_NINA_Lite ArduinoJson
+IF %ERRORLEVEL% NEQ 0 (
+  REM Surf Checker dependencies has not been installed properly.
+)
+
 SET count=0
 FOR /f "tokens=* USEBACKQ" %%i IN (`arduino-cli board list`) do (
   SET /a count=!count!+1
@@ -35,14 +42,16 @@ FOR /f "USEBACKQ" %%i IN (`ECHO !board_list%count_found%!`) do (
 )
 ECHO %nano_com_port%
 
+SET path_to_example=%USERPROFILE%\Documents\Arduino\libraries\IoT-Surf-Checker\examples\wifi_manager
+
 ECHO Starting compilation...
-arduino-cli compile --fqbn arduino:samd:nano_33_iot C:\Users\nicol\Documents\Arduino\libraries\IoT-Surf-Checker\examples\wifi_manager
+arduino-cli compile --fqbn %target% %path_to_example%
 ECHO %ERRORLEVEL% 
 IF %ERRORLEVEL% NEQ 0 (
   ECHO Compilation has failed.
 )
 ECHO Starting upload...
-arduino-cli upload -p %nano_com_port% --fqbn arduino:samd:nano_33_iot C:\Users\nicol\Documents\Arduino\libraries\IoT-Surf-Checker\examples\wifi_manager
+arduino-cli upload -p %nano_com_port% --fqbn %target% %path_to_example%
 ECHO %ERRORLEVEL%
 IF %ERRORLEVEL% NEQ 0 (
   ECHO Upload has failed.
